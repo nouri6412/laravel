1754e460370aeb1ea4ddcfb957caccc0cc55df75
Jalil Nouri <nouri.6412@gmail.com>
2022-09-14 11:48:50 +0430

    بب

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Product extends Model
 {
@@ -11,4 +12,14 @@ class Product extends Model
     protected $fillable = [
         'name', 'detail'
     ];
+
+    public function getNameAttribute($name)
+    {
+        return $name.' '.'paswand';
+    }
+
+    public function setDetailAttribute($value)
+    {
+        $this->attributes['detail'] = Hash::make($value);
+    }
 }
